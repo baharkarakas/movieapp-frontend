fc31e4fcc7accc9ae6fcc1e5715e9adf610ffc2e
Bahar Karakaş <baharkarakas03@gmail.com>
2025-12-21 14:17:28 +0300

    url

--- movieapp-frontend/src/api/http.js
+++ movieapp-frontend/src/api/http.js
@@ -1,5 +1,5 @@
-const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
-
+const BASE_URL = "https://movieapp-backend-bhwe.onrender.com" || "http://localhost:8080";
+console.log("BASE_URL", BASE_URL);
 
 export function getToken() {
   return localStorage.getItem("token");
@@ -44,6 +44,6 @@ export async function apiFetch(path, options = {}) {
   }
 
   return data;
-  console.log("BASE_URL", BASE_URL);
+
 
 }
